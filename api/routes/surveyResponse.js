099e90a9ffe4b5784962bc3c25895aff9bffc711
Hemant <hsinghbisht@salesforce.com>
2020-01-25 13:18:13 +0530

    change in response header.

--- api/routes/surveyResponse.js
+++ api/routes/surveyResponse.js
@@ -12,12 +12,10 @@ router.post('/', (req, res, next) => {
     res.setHeader('Content-type', 'application/json');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'http://' + req.headers.host);
     res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
     res.status(200).json({
         message : 'Handling post request to /surveyResponse.'
     })
-    debugger
 });
 
 router.get('/:surveyResponseId', (req, res, next) => {
