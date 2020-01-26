8967f916e83f13a7dcc081f8123c7a1f0915a404
Hemant <hsinghbisht@salesforce.com>
2020-01-26 20:18:16 +0530

    change in response header.

--- api/routes/surveyResponse.js
+++ api/routes/surveyResponse.js
@@ -15,7 +15,7 @@ router.post('/', (req, res, next) => {
     res.setHeader('Content-type', 'application/json');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', 'https://mail.google.com');
-    res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'www.hemant.bisht@gmail.com');
+    res.setHeader('AMP-Access-Control-Allow-Source-Origin', 'amp@gmail.dev');
     res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
     
 
