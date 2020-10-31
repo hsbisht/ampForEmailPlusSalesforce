ae5138684f43abdc8ec48a60633a8ccb88c42c3d
developertrix <www.hemant.bisht@gmail.com>
2020-10-31 15:17:23 +0530

    test

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ const bodyParser = require('body-parser');
 require("dotenv/config");
 const surveyResponseRoutes = require('./api/routes/feedbackResponse');
 
+/**
+ * Middlewares...
+ */
+
+// Morgan : Used to log the requests..
 app.use(morgan('dev'));
+
+// body-parser: Used to parse data in urlenchoded or json format.
 app.use(bodyParser.urlencoded({
     extended : false
 }));
 app.use(bodyParser.json());
- 
+
 app.use('/feedbackResponse', surveyResponseRoutes);
 
-
+// Error handling....
 app.use((req, res, next) =>{
     const error = new Error('Not Found');
     error.status = 404;
